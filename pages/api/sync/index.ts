2f85514f4edecf3b10f4a86387e9e24436d24baf
Thiago <thiagosalaberry99@gmail.com>
2024-01-04 13:13:20 -0300

    agregamos el lenght

--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -5,11 +5,11 @@ import methods from "micro-method-router";
 import { base } from "../../../lib/airtable";
 import { productsIndex } from "../../../lib/algolia";
 export default methods({
-    async get(req:NextApiRequest, res: NextApiResponse) {
-        const allRecords = await productsIndex.search("false");
-        res.send("cron-job");
-    },
-    async post(req:NextApiRequest, res:NextApiResponse) {
+    // async get(req:NextApiRequest, res: NextApiResponse) {
+    //     const allRecords = await productsIndex.search("false");
+    //     res.send("cron-job");
+    // },
+    async get(req:NextApiRequest, res:NextApiResponse) {
         base("Productos").select({pageSize: 3}).eachPage(async function(records, fetchNextPage) {
             const recordsForAlgolia = records.map(r => {                
                 return {
@@ -20,9 +20,13 @@ export default methods({
             await productsIndex.saveObjects(recordsForAlgolia);
             console.log("Siguiente página");
             fetchNextPage();
-        }, function done(err) {
+        }, async function done(err) {
             if (err) { console.error(err); return ; };
-            res.send("Terminó la sync")
+            const allRecords = await productsIndex.search("false");
+            res.json({
+                message: "Terminó las sync",
+                amount: allRecords.hits.length
+            })
         })
     }
 })
